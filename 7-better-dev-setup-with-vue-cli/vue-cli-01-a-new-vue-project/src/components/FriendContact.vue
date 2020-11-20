9bfe542351e1255899d7f9c2ed46a333c006c353
Matthijs Boet <m.boet@chello.nl>
2020-11-20 22:06:32 +0100

    hekl

--- 7-better-dev-setup-with-vue-cli/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ 7-better-dev-setup-with-vue-cli/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,7 +1,7 @@
 <template>
   <li>
     <h2>{{ friend.name }}</h2>
-    <button @click="toogleDetails">{‌{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <button @click="togleDetails">{‌{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <ul v-if="detailsAreVisible">
       <li><strong>Phone: </strong>{{ friend.phone}}</li>
       <li><strong>Email: </strong>{{ friend.email}}</li>
